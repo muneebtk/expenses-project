7dff1f26b9667a2576b28d89e675df1d084f2001
muhammed muneeb <103311072+muneebtk@users.noreply.github.com>
2025-06-22 17:41:23 +0530

    unnecessary comments removed

--- expense-tracker-frontend/src/App.tsx
+++ expense-tracker-frontend/src/App.tsx
@@ -9,61 +9,20 @@ import ExpenseDetail from "./pages/ExpenseDetailPage";
 import PrivateRoute from "./components/common/PrivateRoute";
 import PublicRoute from "./components/common/PublicRoute";
 
-
 function App() {
-
   return (
     <>
       <Provider store={store}>
         <BrowserRouter>
           <Routes>
-            <Route element={<PublicRoute/>}>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+            <Route element={<PublicRoute />}>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
             </Route>
-            {/* <Route path="/" element={<DashboardPage />} />
-            <Route path="/expenses/:expenseId" element={<ExpenseDetail />} /> */}
-            {/* Protected Routes */}
             <Route element={<PrivateRoute />}>
               <Route path="/" element={<DashboardPage />} />
               <Route path="/expenses/:expenseId" element={<ExpenseDetail />} />
-              {/* Add more protected routes here */}
             </Route>
-
-            {/* Public Routes */}
-            {/* <Route
-              path="/login"
-              element={
-                <PublicRoute>
-                  <LoginPage />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <PublicRoute>
-                  <RegisterPage />
-                </PublicRoute>
-              }
-            />
-
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <DashboardPage />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/expenses/:expenseId"
-              element={
-                <PrivateRoute>
-                  <ExpenseDetail />
-                </PrivateRoute>
-              }
-            /> */}
           </Routes>
         </BrowserRouter>
       </Provider>
