4ae76c80ca8dda31c3cc6d1a0329cdc818633097
muhammed muneeb <103311072+muneebtk@users.noreply.github.com>
2025-06-30 21:21:53 +0530

    sample test case and work flows added

--- expense_track_api/users/tests.py
+++ expense_track_api/users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@mail.com",
+            password="testpassword",
+            first_name="Test",
+            last_name="User",
+        )
+    
+    def test_user_creation(self):
+        user = User.objects.get(email="test@mail.com")
+        self.assertIsNotNone(user)
+        self.assertEqual(user.email, "test@mail.com")
+        self.assertTrue(user.check_password("testpassword"))
+    
